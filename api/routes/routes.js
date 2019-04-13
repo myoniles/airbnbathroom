fe086c44d32e14f9878e369d0c2a2917ef9048a9
Michael T Yoniles <BLMdnst8@gmail.com>
2019-04-13 16:19:49 -0400

    make it actually host the api

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,7 +1,23 @@
 'use strict';
 
+
 module.exports = function(app) {
 	var bnbr_controller = require('../controllers/brController')
+
+	app.route('/')
+		.get( function(req, res){
+			res.sendFile( './web/index.html');
+		});
+	app.route('/google_maps.js')
+		.get( function(req, res){
+			res.sendFile( './web/google_maps.js');
+		});
+	app.route('/style.css')
+		.get( function(req, res){
+			res.sendFile( './web/style.css');
+		});
+
+
 
 	app.route('/bathrooms:brId')
 		.get(bnbr_controller.get_br_info);
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || 3000
+	port = 3000
 	mongoose = require('mongoose')
 	Br_model = require('./api/models/brModel')
 	bodyParser = require('body-parser')
 
 mongoose.Promise = global.Promise
-
 mongoose.connect('mongodb://localhost/airbnbr', { useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,5 +13,7 @@ app.use(bodyParser.json());
 
 var routes = require('./api/routes/routes')
 routes(app)
+
+app.listen(port);
 
 console.log('API launched on port' + port)

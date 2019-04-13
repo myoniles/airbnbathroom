4a1e467a74f5e35302b65b15a59afc6c64a58db3
Michael T Yoniles <BLMdnst8@gmail.com>
2019-04-13 17:10:58 -0400

    more routes

--- api/controllers/brController.js
+++ api/controllers/brController.js
@@ -3,10 +3,38 @@
 var monggoose = require('mongoose'),
 	Br_Model = mongoose.model('Br_Model')
 
+exports.register_br = function(req, res) {
+	var new_br = new Br_Model(req.body);
+	new_br.save(function(err,post){
+		if (err)
+			res.send(err);
+		res.json(post);
+	});
+};
+
 exports.get_br_info = function(req, res) {
 	Br_Model.findById(req.params.brId, function(err, br) {
 		if (err)
 			res.send(err);
 		res.json(br)
+	});
+};
+
+exports.edit_br = function(req, res) {
+	Br_Model.findOneAndUpdate({_id: req.params.brId}, req.body, {new: true}, function(err, br){
+		if (err)
+			res.send(err);
+		res.json(br)
+	});
+};
+
+
+exports.delete_br = function(req, res) {
+	Br_Model.remove({
+		_id: req.params.brId
+	}, function(err, post) {
+			if (err)
+				res.send(err);
+			res.json({ message: 'Bathroom successfully deleted'})
 	});
 };
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -6,20 +6,25 @@ module.exports = function(app) {
 
 	app.route('/')
 		.get( function(req, res){
-			res.sendFile( './web/index.html');
+			res.sendFile( '/home/mike/Documents/proj/airbnbathroom/web/index.html');
 		});
 	app.route('/google_maps.js')
 		.get( function(req, res){
-			res.sendFile( './web/google_maps.js');
+			res.sendFile( '/home/mike/Documents/proj/airbnbathroom/web/google_maps.js');
 		});
 	app.route('/style.css')
 		.get( function(req, res){
-			res.sendFile( './web/style.css');
+			res.sendFile( '/home/mike/Documents/proj/airbnbathroom/web/style.css');
 		});
 
-
-
+	// Routes for a specific bathroom
 	app.route('/bathrooms:brId')
-		.get(bnbr_controller.get_br_info);
+		.get(bnbr_controller.get_br_info)
+		.put(bnbr_controller.edit_br)
+		.delete(bnbr_controller.delete_br);
+
+	// route for abathroom creation
+	app.route('/bathroom')
+		.post(bnbr_controller.register_br);
 
 }
